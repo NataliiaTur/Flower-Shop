7f3ab456ae9893c03027601169c6b1f73428b51b
Nataliia Tur <natalim@ukr.net>
2026-01-30 20:24:45 +0200

    added library mongoose and schema Bouquet

---
+++ src/db/models/bouquet.js
@@ -0,0 +1,40 @@
+import { model, Schema } from 'mongoose';
+
+const bouquetSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    composition: [
+      {
+        flower: String,
+        quantity: Number,
+      },
+    ],
+    price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 5,
+    },
+    colors: [String],
+    description: String,
+
+    images: [
+      {
+        url: { type: String, required: true },
+        alt: String,
+        isMain: { type: Boolean, default: false },
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+export default model('Bouquet', bouquetSchema);
