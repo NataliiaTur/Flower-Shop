137afef4e9ff505204773527f4749a80abefcc24
Nataliia Tur <natalim@ukr.net>
2026-01-30 19:35:17 +0200

    added project's folders, env, envExample, getEnvVar

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { loggerPino } from './middlewares/loggerPino.js';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+
+const PORT = Number(getEnvVar('PORT', 3000));
+
+export const startServer = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(loggerPino);
+
+  app.use((req, res, next) => {
+    console.log(`Time: ${new Date().toLocaleString()}`);
+    next();
+  });
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Pizdec',
+    });
+  });
+
+  app.use(notFoundHandler);
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+  });
+};
