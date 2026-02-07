ae36d4569ba7a80c0f110b7caae35ecf177af716
Nataliia Tur <natalim@ukr.net>
2026-02-07 21:02:05 +0200

    added wrapper validateBody to post flower route

--- src/modules/flowers/flowersRoutes.js
+++ src/modules/flowers/flowersRoutes.js
@@ -7,13 +7,19 @@ import {
   patchFlowerController,
 } from './flowersController.js';
 import { ctrlWrapper } from '../../utils/ctrlWrapper.js';
+import { validateBody } from '../../middlewares/validateBody.js';
+import { createFlowerSchema } from './flowerValidation.js';
 
 const router = Router();
 
 router.get('/catalog', ctrlWrapper(getCatalogController));
 router.get('/catalog/:flowerId', ctrlWrapper(getFlowerByIdController));
 router.delete('/catalog/:flowerId', ctrlWrapper(deleteFlowerController));
-router.post('/catalog', ctrlWrapper(postFlowerController));
+router.post(
+  '/catalog',
+  validateBody(createFlowerSchema),
+  ctrlWrapper(postFlowerController),
+);
 router.patch('/catalog/:flowerId', ctrlWrapper(patchFlowerController));
 
 export default router;
